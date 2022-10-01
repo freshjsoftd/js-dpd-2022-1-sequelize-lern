d43c2227eafb2da149d0b1616c16e26cff067d07
yan <yan.braverman37@gmail.com>
2022-10-01 14:04:19 +0300

    Add mongoDB and DB main

---
+++ packages/server/src/constants/mongo-constants.js
@@ -0,0 +1,129 @@
+export const customers = [
+  {
+    firstName: 'Nick',
+    lastName: 'Myson',
+    age: 50,
+    prog_langs: ['JS', 'C#', 'Java'],
+    address: {
+      country: 'USA',
+      city: 'New York',
+      street: 'Wholt Street',
+      building: '722',
+      app: '250',
+      zipcode: 10020,
+    },
+  },
+  {
+    firstName: 'Harry',
+    lastName: 'Porter',
+    age: 45,
+    prog_lang: ['JS', 'Python', 'Java'],
+    address: {
+      country: 'USA',
+      city: 'Manchester',
+      street: 'Robin Hood',
+      building: '320d',
+      app: '32',
+      zipcode: 03105,
+    },
+  },
+  {
+    firstName: 'Ken',
+    lastName: 'Hensly',
+    age: 70,
+    prog_lang: ['JS', 'Kotlin', 'SQL'],
+    address: {
+      country: 'Great Britain',
+      city: 'London',
+      street: 'Backer Street',
+      zipcode: 5208
+    },
+  },
+  {
+    firstName: 'Mike',
+    lastName: 'Tyson',
+    age: 50,
+    prog_lang: ['HTML', 'CSS', 'XML'],
+    address: {
+      country: 'USA',
+      city: 'Washington',
+      street: 'Wholt Street',
+      building: '20 b',
+      app: '120',
+      zipcode: 98002,
+    },
+  },
+  {
+    firstName: 'Michael',
+    lastName: 'Jordan',
+    age: 45,
+    prog_lang: ['Scala', 'Go', 'Ruby'],
+    address: {
+      country: 'USA',
+      city: 'Chicago',
+      street: 'Rusvelt Teodor',
+      building: '20 b',
+      app: '120',
+      zipcode: 60176,
+    },
+  },
+  {
+    firstName: 'Paul',
+    lastName: 'MacCartney',
+    age: 70,
+    prog_lang: ['Ruby', 'Python', 'Pascal'],
+    address: {
+      country: 'Great Britain',
+      city: 'London',
+      street: 'Trafalgar squear',
+    },
+  },
+  {
+    firstName: 'Jhon',
+    lastName: 'Doe',
+    age: 30,
+    prog_lang: ['JS', 'Python', 'Algol67'],
+    address: {
+      country: 'Ukraine',
+      city: 'Dnipro',
+      street: 'Yavornitskogo',
+      building: '18b',
+      app: '120',
+      zipcode: 49130,
+    },
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Smith',
+    age: 25,
+    prog_lang: ['CoffeeScript', 'Java', 'Fortran'],
+    address: {
+      country: 'Ukraine',
+      city: 'Zaporizhzhia',
+      street: 'Svobody',
+      building: '20 b',
+      app: '120',
+      zipcode: 69061,
+    },
+  },
+  {
+    firstName: 'Benya',
+    lastName: 'Crick',
+    age: 40,
+    prog_lang: ['TypeScript', 'Python', 'Assembler'],
+    address: {
+      country: 'Ukraine',
+      city: 'Odessa',
+      street: 'Deribasovskaya',
+      building: '20 b',
+      app: '120',
+      zipcode: 65125
+    },
+  }
+]
+
+export const roles = [
+  {type: 'user'},
+  {type: 'moderator'},
+  {type: 'admin'}
+]
